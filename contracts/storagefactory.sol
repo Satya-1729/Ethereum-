1b7be3faf6ba7ad52343b8fa1f64b1ae2dfd4919
Satya-1729 <satya1729kumar@gmail.com>
2024-07-31 21:20:17 +0530

    intial commit

--- contracts/storagefactory.sol
+++ contracts/storagefactory.sol
@@ -12,9 +12,27 @@ contract storagefactory{
     // uint256 public favoritenumber;
     // visiblity type public
 
-    Simple_Storage public mysimplestorage;
+    // Simple_Storage public mysimplestorage; its been overidden the simplestorage contract variable
+    Simple_Storage[] public listofmysimplestoragecontracts;
+
     function createsimplestoragecontract() public{
-        mysimplestorage = new Simple_Storage();
+        Simple_Storage newsimplestoragecontract = new Simple_Storage();
+        listofmysimplestoragecontracts.push(newsimplestoragecontract);
+    
+
+    }
+
+    function sfstore(uint256 _simplestorageindex, uint256 _simplestoragenumber) public{
+        //address
+        //ABI
+        // Simple_Storage mysimplestorage = listofmysimplestoragecontracts[_simplestorageindex];
+        listofmysimplestoragecontracts[_simplestorageindex].store(_simplestoragenumber);
+
+    }
+
+    function sfget(uint256 _simplestorageindex) public view returns(uint256){
+        // Simple_Storage mysimplestorage = listofmysimplestoragecontracts[_simplestorageindex];
+        return listofmysimplestoragecontracts[_simplestorageindex].retrieve();
 
     }
     

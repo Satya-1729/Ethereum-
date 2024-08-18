1ad9fe4b504b03575951dfc00386310ddc2968cd
Satya-1729 <satya1729kumar@gmail.com>
2024-08-18 11:42:29 +0530

    scriptfile

---
+++ foundry_f24/script/DeployingSimpleStorage.s.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identtifier:MIT
+pragma solidity ^0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+import {SimpleStorage} from "../src/SimpleStorage.sol";
+
+contract DeploySimpleStorage is Script {
+    function run() external returns (SimpleStorage) {
+        vm.startBroadcast();
+        SimpleStorage simplestoraage = new SimpleStorage();
+        vm.stopBroadcast();
+        return simplestoraage;
+    }
+}
